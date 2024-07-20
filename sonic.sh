67f445784702b69821375ee32507b3948d6c44bf
Enes <gunesenes034@gmail.com>
2024-07-20 17:25:40 +0300

    Update sonic.sh

--- sonic.sh
+++ sonic.sh
@@ -3,8 +3,7 @@
 BOLD_BLUE='\033[1;34m'
 NC='\033[0m'
 echo
-if ! command -v node &> /dev/null
-then
+if ! command -v node &> /dev/null; then
     echo -e "${BOLD_BLUE}Node.js is not installed. Installing Node.js...${NC}"
     echo
     curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -13,8 +12,7 @@ else
     echo -e "${BOLD_BLUE}Node.js is already installed.${NC}"
 fi
 echo
-if ! command -v npm &> /dev/null
-then
+if ! command -v npm &> /dev/null; then
     echo -e "${BOLD_BLUE}npm is not installed. Installing npm...${NC}"
     echo
     sudo apt-get install -y npm
@@ -24,7 +22,7 @@ fi
 echo
 echo -e "${BOLD_BLUE}Creating project directory and navigating into it${NC}"
 mkdir -p SonicBatchTx
-cd SonicBatchTx
+cd SonicBatchTx || exit
 echo
 echo -e "${BOLD_BLUE}Initializing a new Node.js project${NC}"
 echo
@@ -45,34 +43,32 @@ import web3 from "@solana/web3.js";
 import chalk from "chalk";
 import bs58 from "bs58";
 
-const connection = new web3.Connection("https://devnet.sonic.game", 'confirmed');
+const connection = new web3.Connection("https://api.devnet.solana.com", 'confirmed');
 
 const privkey = "$privkey";
 const from = web3.Keypair.fromSecretKey(bs58.decode(privkey));
 const to = web3.Keypair.generate();
 
 (async () => {
-    const transaction = new web3.Transaction().add(
-        web3.SystemProgram.transfer({
-          fromPubkey: from.publicKey,
-          toPubkey: to.publicKey,
-          lamports: web3.LAMPORTS_PER_SOL * 0.001,
-        }),
-      );
-    
-      
-      const txCount = 100;
-      for (let i = 0; i < txCount; i++) {
-      const signature = await web3.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [from],
-      );
-    console.log(chalk.blue('Tx hash :'), signature);
-    console.log("");
-    const randomDelay = Math.floor(Math.random() * 3) + 1;
-    await new Promise(resolve => setTimeout(resolve, randomDelay * 1000));
-  }
+    try {
+        for (let i = 0; i < 100; i++) {
+            const transaction = new web3.Transaction().add(
+                web3.SystemProgram.transfer({
+                    fromPubkey: from.publicKey,
+                    toPubkey: to.publicKey,
+                    lamports: web3.LAMPORTS_PER_SOL * 0.001,
+                })
+            );
+
+            const signature = await web3.sendAndConfirmTransaction(connection, transaction, [from]);
+            console.log(chalk.blue('Tx hash :'), signature);
+
+            const randomDelay = Math.floor(Math.random() * 3) + 1;
+            await new Promise(resolve => setTimeout(resolve, randomDelay * 1000));
+        }
+    } catch (error) {
+        console.error(chalk.red('Error during transaction:'), error);
+    }
 })();
 EOF
 echo
